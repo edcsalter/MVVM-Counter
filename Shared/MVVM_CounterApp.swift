cbdb540032a7e2a1ec5e68880711e7ff6e2dce74
MadArkitekt <edcsalter@gmail.com>
2022-03-10 11:09:03 -0500

    Setup

---
+++ Shared/MVVM_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CounterApp.swift
+//  Shared
+//
+//  Created by Ed Salter on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
